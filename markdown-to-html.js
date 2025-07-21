c1b870b598e485526970a888b45400281ea11756
AashmanShukla3223 <aashmanshukla323@gmail.com>
2025-07-21 21:29:35 +0530

    Delete markdown-to-html.js

--- markdown-to-html.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('convert-btn').addEventListener('click', function() {
-    const markdownInput = document.getElementById('markdown-input').value;
-    const htmlOutput = document.getElementById('html-output');
-    const converter = new showdown.Converter();
-    
-    htmlOutput.value = converter.makeHtml(markdownInput);
-}); 
