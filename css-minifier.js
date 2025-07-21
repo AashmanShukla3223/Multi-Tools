9fc9e491d3da2d608f7a38197955a52345f9fae6
AashmanShukla3223 <aashmanshukla323@gmail.com>
2025-07-21 21:20:47 +0530

    Delete css-minifier.js

--- css-minifier.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('minify-btn').addEventListener('click', function() {
-    let css = document.getElementById('css-input').value;
-    
-    css = css.replace(/\/\*[\s\S]*?\*\/|\/\/.*/g, ''); // remove comments
-    css = css.replace(/\s+/g, ' '); // collapse whitespace
-    css = css.replace(/\s*([;:{},])\s*/g, '$1'); // remove space around separators
-
-    document.getElementById('minified-css').value = css.trim();
-}); 
