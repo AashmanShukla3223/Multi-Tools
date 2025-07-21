28fb5d7ded05a9df63753bb2cdc3172694037771
AashmanShukla3223 <aashmanshukla323@gmail.com>
2025-07-21 21:29:44 +0530

    Delete md5-hash-generator.js

--- md5-hash-generator.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('generate-btn').addEventListener('click', function() {
-    const textInput = document.getElementById('text-input').value;
-    const output = document.getElementById('output');
-    
-    output.value = CryptoJS.MD5(textInput).toString();
-}); 
