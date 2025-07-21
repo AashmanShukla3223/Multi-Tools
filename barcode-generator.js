3330e2f795cb5685d152b49be72a27486e2c4765
AashmanShukla3223 <aashmanshukla323@gmail.com>
2025-07-21 21:19:20 +0530

    Delete barcode-generator.js

--- barcode-generator.js
+++
@@ -1,6 +0,0 @@
-document.getElementById('generate-btn').addEventListener('click', function() {
-    const text = document.getElementById('text-input').value;
-    if (text) {
-        JsBarcode("#barcode", text);
-    }
-}); 
