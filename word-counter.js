ff13ce503e1ce42e526c72a7f8a4affbfdd4e492
AashmanShukla3223 <aashmanshukla323@gmail.com>
2025-07-21 21:21:59 +0530

    Delete word-counter.js

--- word-counter.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('text-input').addEventListener('input', function() {
-    const text = this.value;
-    const resultDiv = document.getElementById('result');
-    const words = text.trim().split(/\s+/).filter(word => word.length > 0);
-    const wordCount = words.length;
-
-    resultDiv.innerHTML = `<div class="alert alert-info">Word count: ${wordCount}</div>`;
-}); 
